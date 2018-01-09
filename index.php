ae66eca17a16483329763e55161321069597e827
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-09 10:53:43 -0700

    added header 2

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 		<h1>This Page Is All About Isaiah</h1>
 		<img src="isaiahFunnyFace.jpg" alt = "my funny face" />
 		<p>I believe humor is the key to living a happy, healthy life</p>
-		<p>These are a <em>few</em> of the careers I have had:</p>
+		<p>These are a <em>few</em> of the careers I have had <strong>(none of which were humorous)</strong>:</p>
 		<ul>
 			<li>Aviation Maintenance Technician</li>
 			<li>Entitlement Program Director</li>
 			<li>Facilities Technologist</li>
 			<li>Lead Actor - Movie Pilot (Can't remember name)</li>
 		</ul>
+
+		<h2>My Family</h2>
+		<p>I'm the youngest of 2 brothers and 2 sisters. I have 1 kid who is 9 years old. He stay home alone while
+			I'm at bootcamp but he should be fine. I leave him with snacks and the Discovery Channel!</p>
 	</body>
 </html>
