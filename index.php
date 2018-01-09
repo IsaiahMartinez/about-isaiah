0c8f6775bd216d47882106717b2b3e90ec4c29a6
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-09 10:24:36 -0700

    created index.php file

--- index.php
+++ index.php
@@ -6,5 +6,14 @@
 	</head>
 	<body>
 		<h1>This Page Is All About Isaiah</h1>
+		<img src="" />
+		<p>I believe humor is the key to living a happy, healthy life</p>
+		<p>These are a <em>few</em> of the careers I have had:</p>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
 	</body>
 </html>
