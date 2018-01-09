a4a5fdaf73348b6974bf07d438ca7b4fc74e5474
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-09 09:51:18 -0700

    created index.php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>All About Isaiah</title>
+	</head>
+	<body>
+		<h1>This Page Is All About Isaiah</h1>
+	</body>
+</html>
